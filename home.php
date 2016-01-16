a885fe79bba77c640e41ec23516c532700dea99e
Stephanie Ogden <stephogden4@gmail.com>
2016-01-16 12:58:49 -0700

    Home page with Assignment page

---
+++ home.php
@@ -0,0 +1,22 @@
+<body>
+<header><h1>Stephanie Ogden</h1></header>
+<main>
+    <h1>About Me</h1>
+
+    <aside>
+
+        <?php
+echo "<h2>Family</h2>";
+echo "I have 4 beautiful children and a very hard working husband<br>";
+echo "<h2>Career</h2>";
+echo "I work at an insurance software company called Xactware<br>";
+echo "<h2>Hobbies</h2>";
+echo "I like to play the guitar";
+?>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
